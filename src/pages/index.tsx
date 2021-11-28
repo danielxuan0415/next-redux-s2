97ce42205b665ea6c3352cb144d06b2ab0c193d6
danielxuan0415 <arminiodurando@gmail.com>
2021-11-28 15:15:09 -0500

    add menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,11 @@
-import React, { useState } from "react"
-import { useAppDispatch, useAppSelector } from "../app/hooks"
-import { selectCount } from "../features/counter/counterSlice";
+import Link from 'next/link';
 
-import { increment, decrement, incrementByAmount }
-    from "../features/counter/counterSlice";
-const val1: number = 0
 const IndexPage = () => {
-    const dispatch = useAppDispatch();
-    const count = useAppSelector(selectCount);
-    const [incrementAmount, setIncrementAmount] = useState<number>(0);
-
     return (
         <>
-            <h1>Welcome to the greatest app in the world!</h1>
-            <h2>The current number is {count}</h2>
-            <div>
-                <input type="text" name="amount" value={incrementAmount} onChange={(e) => setIncrementAmount(Number(e.target.value))} />
-                <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>Increment by Amount</button>
-            </div>
-            <div>
-                <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
-                <button onClick={() => dispatch(increment())}>Increment by 1</button>
-            </div>
+            <Link href='/counter'><a>Counter Page!</a></Link>{' '}
+            <Link href='/quote'><a>Quote Page!</a></Link>
         </>
-    )
-}
-export default IndexPage
+    );
+};
+export default IndexPage;
